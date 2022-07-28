be50c450a48fabddfef8772a85c8feeb7a69f5dc
nikilneosoft <nikil.kurian@neosoftmail.com>
2022-07-28 12:53:24 +0530

    using map

--- angular-posts/src/app/pages/posts/posts.component.ts
+++ angular-posts/src/app/pages/posts/posts.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IPost } from 'src/app/models/IPost';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit {
-
-  constructor() { }
+export class PostsComponent implements OnInit, OnDestroy {
+  posts:IPost[]=[];
+  postSubscription!:Subscription;
+  constructor(private postService:PostService) { }
 
   ngOnInit(): void {
+    this.postSubscription = this.postService.getPosts().subscribe((posts)=>{
+       this.posts = posts
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.postSubscription && this.postSubscription.unsubscribe();
   }
 
 }
--- angular-posts/src/app/services/post.service.ts
+++ angular-posts/src/app/services/post.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPost } from '../models/IPost';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,13 @@ export class PostService {
   constructor(private http:HttpClient) { }
 
   getPosts(){
-    return this.http.get(`https://rxjs-posts-default-rtdb.firebaseio.com/posts.json`);
+    return this.http.get<{[id:string]:IPost}>(`https://rxjs-posts-default-rtdb.firebaseio.com/posts.json`).pipe(map(posts=>{
+      let postsData:IPost[]=[];
+      for(let id in posts){
+        postsData.push({...posts[id],id});
+        console.log("postData",posts[id]);
+      }      
+      return postsData;
+    }));
   }
 }
