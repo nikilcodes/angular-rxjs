1a4d367fff2e0223c31deb9a614f50c38403be1f
nikilneosoft <nikil.kurian@neosoftmail.com>
2022-08-02 17:25:56 +0530

    Change detection strategy

--- angular-posts/src/app/pages/posts/posts.component.ts
+++ angular-posts/src/app/pages/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ComponentRef, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IPost } from 'src/app/models/IPost';
 import { PostService } from 'src/app/services/post.service';
@@ -6,16 +6,18 @@ import { PostService } from 'src/app/services/post.service';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
+  changeDetection:ChangeDetectionStrategy.OnPush,
 })
 export class PostsComponent implements OnInit, OnDestroy {
   posts:IPost[]=[];
   postSubscription!:Subscription;
-  constructor(private postService:PostService) { }
+  constructor(private postService:PostService,private ref:ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.postSubscription = this.postService.getPostsWithCategory().subscribe((posts)=>{
-       this.posts = posts
+       this.posts = posts;
+       this.ref.detectChanges();
     })
   }
 
